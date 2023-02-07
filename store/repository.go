76670b3cde9976bf5248ef3e4ebd0dddfb272b09
Eitaro <e.yamatsuta49@gmail.com>
2023-02-08 06:59:05 +0900

    feat: add interfaces

--- store/repository.go
+++ store/repository.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"github.com/eitarox/todo-app-api/clock"
 	"time"
 
 	"github.com/eitarox/todo-app-api/config"
@@ -29,3 +30,36 @@ func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
 	xdb := sqlx.NewDb(db, "mysql")
 	return xdb, func() { _ = db.Close() }, nil
 }
+
+type Repository struct {
+	Clocker clock.Clocker
+}
+
+type Beginner interface {
+	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
+}
+
+type Preparer interface {
+	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
+}
+
+type Execer interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
+}
+
+type Queryer interface {
+	Preparer
+	QueryxContext(ctx context.Context, query string, args ...any) (*sqlx.Rows, error)
+	QueryRowxContext(ctx context.Context, query string, args ...any) *sqlx.Row
+	GetContext(ctx context.Context, dest interface{}, query string, args ...any) error
+	SelectContext(ctx context.Context, dest interface{}, query string, args ...any) error
+}
+
+var (
+	_ Beginner = (*sqlx.DB)(nil)
+	_ Preparer = (*sqlx.DB)(nil)
+	_ Queryer  = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.DB)(nil)
+	_ Execer   = (*sqlx.Tx)(nil)
+)
