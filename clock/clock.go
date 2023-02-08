f2f96a984d600509ccf680f7895e853683fca6e7
Eitaro <e.yamatsuta49@gmail.com>
2023-02-09 01:16:27 +0900

    test: add test

--- clock/clock.go
+++ clock/clock.go
@@ -5,3 +5,15 @@ import "time"
 type Clocker interface {
 	Now() time.Time
 }
+
+type RealClocker struct{}
+
+func (r RealClocker) Now() time.Time {
+	return time.Now()
+}
+
+type FixedClocker struct{}
+
+func (fc FixedClocker) Now() time.Time {
+	return time.Date(2023, 2, 8, 20, 57, 34, 0, time.UTC)
+}
